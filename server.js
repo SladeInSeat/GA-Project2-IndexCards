9501198a920c24796bec4877d2e5462581549b05
Slade Sawyer <Slade@Slades-MacBook-Air.local>
2019-04-25 17:13:42 -0400

    npm installed dependencies

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const methodOverride = require('method-override')
+const routes = require('./routes/index')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(methodOverride('_method'))
+
+app.set('view engine', 'hbs')
+app.use(express.static(__dirname + '/public'))
+
+
+app.use('/', routes)
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log("connected at port: " + PORT);
+})
+
