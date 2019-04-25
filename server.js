66ddcf81f05e2b89bd9f84e7973405ae08115b3f
Slade Sawyer <Slade@Slades-MacBook-Air.local>
2019-04-25 17:29:16 -0400

    changed requirement in server.js of routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
-const routes = require('./index.js')
+const routes = require('./index')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
