66968d5f77d9de43879f8b81b8fcc34cf0d98ef3
Slade Sawyer <Slade@Slades-MacBook-Air.local>
2019-04-25 18:49:28 -0400

    created app file structure

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 let express = require('express')
 let router = express.Router()
+// require user, topic, card apis here
 
 router.get('/',function(req,res){
     res.send('<h1>heroku test</h1>');
 });
 
-module.exports = router
+module.exports = router 
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
-const routes = require('./index')
+const routes = require('./routes/index')
 
+const methodOverride = require('method-override')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(methodOverride('_method'))
+
+app.set('view engine', 'hbs')
+
+app.use(express.static(__dirname + '/public'))
 
 app.use('/', routes)
 
