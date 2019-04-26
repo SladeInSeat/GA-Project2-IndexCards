936b89d1c2c2e5bd7e46abcea8a90500fca6798a
Slade Sawyer <Slade@Slades-MacBook-Air.local>
2019-04-26 17:24:38 -0400

    changed apis/users.js

--- apis/users.js
+++ apis/users.js
@@ -3,8 +3,7 @@ const User = require('../models/User.js')
 const userApi = {
 
     renderAllUsers: function(req,res){
-        User.find().
-        then(allUsers => {
+        User.find().then(allUsers => {
             res.render('../views/users', {allUsers})
         })
     }
