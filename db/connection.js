075073cefbde4f8efd2090e38cb1b570806d1b21
Slade Sawyer <Slade@Slades-MacBook-Air.local>
2019-04-26 17:41:52 -0400

    added connection code from heroku lesson, thanks supriya

--- db/connection.js
+++ db/connection.js
@@ -1,25 +1,34 @@
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://sladesawyer:thudnob78@testcluster-81fgp.mongodb.net/test?retryWrites=true";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
-
 // requirements: import mongoose
 mongoose = require('mongoose');
 
 // Connect to a local database called "the-donut-shop"
 // When it connects, then console.log "Connected to MongoDB"
 
-const connectionString = "mongodb+srv://sladesawyer:thudnob78@testcluster-81fgp.mongodb.net/test?retryWrites=true"
+// const connectionString = "mongodb+srv://sladesawyer:thudnob78@testcluster-81fgp.mongodb.net/test?retryWrites=true"
 // const connectionString = "mongodb://localhost/indexCards"
-mongoose.connect(connectionString, {useNewURLParser: true})
-    .then(() => {
-        console.log('connected to mongo at ' + connectionString);
-    });
+// mongoose.connect(connectionString, {useNewURLParser: true})
+//     .then(() => {
+//         console.log('connected to mongo at ' + connectionString);
+//     });
 
-// export your mongoose connection
 
-module.exports = mongoose;
+
+
+
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+  }
+  else {
+    mongoose.connect('mongodb://localhost/indexCards');
+  }
+  mongoose.connection.on('error', function(err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+    }
+  );
+  mongoose.connection.once('open', function() {
+    console.log("Mongoose has connected to MongoDB!");
+  });
+
+  // export your mongoose connection
+  module.exports = mongoose;
