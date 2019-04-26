ecde6768e7ab2f20efe10f7a07d4594c5bca3309
Slade Sawyer <Slade@Slades-MacBook-Air.local>
2019-04-25 21:04:34 -0400

    added db folder, connections and seeds files

---
+++ db/connection.js
@@ -0,0 +1,25 @@
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://sladesawyer:thudnob78@testcluster-81fgp.mongodb.net/test?retryWrites=true";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+// requirements: import mongoose
+mongoose = require('mongoose');
+
+// Connect to a local database called "the-donut-shop"
+// When it connects, then console.log "Connected to MongoDB"
+
+const connectionString = "mongodb+srv://sladesawyer:<password>@testcluster-81fgp.mongodb.net/test?retryWrites=true"
+
+mongoose.connect(connectionString, {useNewURLParser: true})
+    .then(() => {
+        console.log('connected to mongo at ' + connectionString);
+    });
+
+// export your mongoose connection
+
+module.exports = mongoose;
