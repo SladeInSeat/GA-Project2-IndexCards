cbe3c09a2f4bd5da03a3d434dec66b7cc8e7016c
Slade Sawyer <Slade@Slades-MacBook-Air.local>
2019-04-26 17:04:16 -0400

    removed old function from apis/users.js, added views folder - this is probably why heroku wasnt working

--- apis/users.js
+++ apis/users.js
@@ -1,14 +1,7 @@
 const User = require('../models/User.js')
 
 const userApi = {
-    showAllUsers: function(req,res){
-        User.find().
-        then(users =>{
-            res.show(users);
-            }
-        )
-    },
-    
+
     renderAllUsers: function(req,res){
         User.find().
         then(allUsers => {
