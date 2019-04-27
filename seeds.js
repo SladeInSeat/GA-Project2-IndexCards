a1ffdac452dce821cbbe1d7510d5ea1695b8242b
Slade Sawyer <Slade@Slades-MacBook-Air.local>
2019-04-26 20:51:13 -0400

    seeds shifting comments around to find bug, on last part

--- seeds.js
+++ seeds.js
@@ -35,55 +35,55 @@ if (process.env.MONGODB_URI) {
 //         console.log('created these users:' + newusers)
 //     });
 
-User.find()
-    .then(users => {
-    Topic.create({topicName: 'HTML',
-                    topicDescription: 'Definitions of common HTML commands and tags',
-                    parentUser: users[0]._id,
-                    lastCompletedDate: Date.now()
-                },
-                {topicName:'Javascript',
-                    topicDescription: 'Vanilla javascript datatypes and functions',
-                    parentUser: users[0]._id,
-                    lastCompletedDate: Date.now()
-                })
-    .then(newtopics => {
-        console.log('created these topics: ' + newtopics)
+// User.find()
+//     .then(users => {
+//     Topic.create({topicName: 'HTML',
+//                     topicDescription: 'Definitions of common HTML commands and tags',
+//                     parentUser: users[0]._id,
+//                     lastCompletedDate: Date.now()
+//                 },
+//                 {topicName:'Javascript',
+//                     topicDescription: 'Vanilla javascript datatypes and functions',
+//                     parentUser: users[0]._id,
+//                     lastCompletedDate: Date.now()
+//                 })
+//     .then(newtopics => {
+//         console.log('created these topics: ' + newtopics)
+//     })
+// })
+
+Topic.find().then( topics => {
+    Card.create({
+            cardPrompt: 'img',
+            cardAnswer: 'image tag, inserts an image file into page. Syntax: <img src=<path to image file> *alt=<path to backup file> *width=<unit> *heigth=<unit>',
+            correctAnswerStreak: 0,
+            lastStreakDate: Date.now(),
+            parentTopic: topics[0]._id
+        },
+        {   
+            cardPrompt: 'br',
+            cardAnswer: 'Inserts a line break into the page. No closing tag',
+            correctAnswerStreak: 0,
+            lastStreakDate: Date.now(),
+            parentTopic: topics[0]._id
+        },
+        {   
+            cardPrompt: 'object',
+            cardAnswer: 'A collection of properties in key-value pairs. Keys must be strings. Values can be anything, even functions',
+            correctAnswerStreak: 0,
+            lastStreakDate: Date.now(),
+            parentTopic: topics[1]._id
+        },
+        {
+            cardPrompt: '.push',
+            cardAnswer: 'A method of Arrays. Adds one or more element to the end of calling array. array.push(<element>)',
+            correctAnswerStreak: 0,
+            lastStreakDate: Date.now(),
+            parentTopic: topics[1]._id
+        })
+    .then(cards => {
+        console.log('created these cards' + cards)
     })
 })
-
-// Topic.find().then( topics => {
-//     Card.create(    
-//         {
-//             cardPrompt: 'img',
-//             cardAnswer: 'image tag, inserts an image file into page. Syntax: <img src=<path to image file> *alt=<path to backup file> *width=<unit> *heigth=<unit>',
-//             correctAnswerStreak: 0,
-//             lastStreakDate: Date.now(),
-//             parentTopic: topics[0]._id
-//         },
-//         {   
-//             cardPrompt: 'br',
-//             cardAnswer: 'Inserts a line break into the page. No closing tag',
-//             correctAnswerStreak: 0,
-//             lastStreakDate: Date.now(),
-//             parentTopic: topics[0]._id
-//         },
-//         {   
-//             cardPrompt: 'object',
-//             cardAnswer: 'A collection of properties in key-value pairs. Keys must be strings. Values can be anything, even functions',
-//             correctAnswerStreak: 0,
-//             lastStreakDate: Date.now(),
-//             parentTopic: topics[1]._id
-//         },
-//         {
-//             cardPrompt: '.push',
-//             cardAnswer: 'A method of Arrays. Adds one or more element to the end of calling array. array.push(<element>)',
-//             correctAnswerStreak: 0,
-//             lastStreakDate: Date.now(),
-//             parentTopic: topics[1]._id
-//         })
-//     .then(cards => {
-//         console.log('created these cards' + cards)});
-// })
 
 
