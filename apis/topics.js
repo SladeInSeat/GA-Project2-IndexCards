2bf6d2da8ffd1d2b0610707b5a2f5640595c809e
Slade Sawyer <Slade@Slades-MacBook-Air.local>
2019-04-28 13:32:16 -0400

    rewrote topics.js

--- apis/topics.js
+++ apis/topics.js
@@ -1,17 +1,16 @@
 const Topic = require("../models/Topic.js")
 
 const topicApi = {
+
     renderAllTopics: function (req,res){
-        Topic.find().then( allTopics => {
+        Topic.find().then(allTopics => {
             res.render("../views/showTopics", {allTopics})
-        })
+        });
     },
 
     renderSingleTopic: function(req,res){
         Topic.findById(req.params.topicId)
     },
-
-
 };
 
 module.exports = topicApi;
