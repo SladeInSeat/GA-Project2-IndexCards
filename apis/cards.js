2beeec8b286a43b51894378068d7afe0d0705c19
Slade Sawyer <Slade@Slades-MacBook-Air.local>
2019-04-29 16:43:51 -0400

    edit card funcitonality; added function to cards.js, added view editCard.hbs

--- apis/cards.js
+++ apis/cards.js
@@ -2,43 +2,56 @@ const Card = require("../models/Card.js")
 
 const cardApi = {
 
-    renderAllCards: function(req,res){
-        Card.find({parentTopic : req.params.topicId }).then( allCards => {
-            res.render("../views/showCards", {allCards})
+    renderAllCards: function (req, res) {
+        Card.find({ parentTopic: req.params.topicId }).then(allCards => {
+            res.render("../views/showCards", { allCards })
         });
     },
 
-    renderSingleCard: function(req,res){
-        Card.findById(req.params.cardId).then( card => {
-            res.render("../views/showSingleCard", {card})
+    renderSingleCard: function (req, res) {
+        Card.findById(req.params.cardId).then(card => {
+            res.render("../views/showSingleCard", { card })
         })
     },
 
-    renderCreateCard: function(req,res){
+    renderCreateCard: function (req, res) {
         let topicId = req.params.topicId
-        res.render("../views/createCard", {topicId})
+        res.render("../views/createCard", { topicId })
     },
 
-    createCard: function(req,res){
-        Card.create({cardPrompt: req.body.cardPrompt,
+    createCard: function (req, res) {
+        Card.create({
+            cardPrompt: req.body.cardPrompt,
             cardAnswer: req.body.cardAnswer,
             correctAnswerStreak: 0,
             lastStreakDate: Date.now(),
-            parentTopic: req.params.topicId}).then( card => {
-                res.render("../views/showSingleCard", {card})
-            });
+            parentTopic: req.params.topicId
+        }).then(card => {
+            res.render("../views/showSingleCard", { card })
+        });
     },
 
-    deleteCard: function(req,res){
-        Card.findByIdAndDelete(req.body._id).then( () => {
+    deleteCard: function (req, res) {
+        Card.findByIdAndDelete(req.body._id).then(() => {
             res.redirect(`/`)
         })
     },
 
-    renderEditCard: function(req,res){
-        Card.findById(req.params.cardId).then( card => {
-            res.render("../views/editCard", {card})
+    renderEditCard: function (req, res) {
+        Card.findById(req.params.cardId).then(card => {
+            res.render("../views/editCard", { card })
         })
+    },
+
+    updateCard: function (req, res) {
+        Card.findByIdAndUpdate(req.params.cardId, {
+            $set: {
+                cardPrompt: req.body.cardPrompt,
+                cardAnswer: req.body.cardAnswer
+            }
+        }, { new: true }).then(() => {
+            res.redirect("/")
+        });
     }
 
 }
