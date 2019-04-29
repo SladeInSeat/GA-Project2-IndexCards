571c9c4275a9fcedceec55bee9873358a76139e9
Slade Sawyer <Slade@Slades-MacBook-Air.local>
2019-04-29 13:46:12 -0400

    added find() criteria to find only cards to specific topic

--- apis/cards.js
+++ apis/cards.js
@@ -3,7 +3,7 @@ const Card = require("../models/Card.js")
 const cardApi = {
 
     renderAllCards: function(req,res){
-        Card.find().then( allCards => {
+        Card.find({parentTopic : req.params.topicId }).then( allCards => {
             res.render("../views/showCards", {allCards})
         });
     }
